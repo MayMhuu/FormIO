2c2815fecd8c65ab47bf066c4c46580f0d165ae9
maymhuu <maymyatmon@ayainnovation.com>
2020-08-18 14:47:03 +0630

    add api call function

--- src/App.js
+++ src/App.js
@@ -3,75 +3,103 @@ import { Route } from 'react-router-dom'
 import Header from './containers/Header'
 import Footer from './containers/Footer'
 import Home from './views/Home'
-//import Form from './views/Form'
-//import Event from './vews/Auth/Auth'
+import Form from './views/Form'
+import Event from './views/Event'
+import Auth from './views/Auth/Auth'
 import { AppConfig } from './config';
-import { Form } from 'react-formio';
+import { Form as FormIO } from 'react-formio';
 import { FormBuilder } from 'react-formio';
 import { Alert } from 'react-alert'
 
 const App = () => (
+
   <div>
-    <Header />
+    {/* <Header /> */}
 
 
     <div className="container" id="main">
-      {/* { AppConfiews/Event'
-import Auth from './viig.projectUrl === 'https://xojgwtxalpylmpd.form.io' ?
+      {/* { AppConfig.projectUrl === 'https://reactstarter.form.io' ?
         <div className="alert alert-warning">This app is still configured to use the default project. Be sure to create your own project in form.io and change the PROJECT_URL in src/config.js</div>
         : null
-      } 
-       <Route exact path="/" component={Home} />
+      }
+      
+      <Route exact path="/" component={Home} />
       <Route path="/form" component={Form} />
       <Route path="/event" component={Event} />
       <Route path="/auth" component={Auth} /> */}
-      {/* <h2>Person Data</h2>
-      <Form src="https://xojgwtxalpylmpd.form.io/person" onSubmit={
-        function (submission) {
-          console.log(submission)
-        }
-      } /> */}
-      <h2>Customer Data</h2>
+      
+      <h2>Person Data</h2>
+      <FormIO src="https://xojgwtxalpylmpd.form.io/registeration" onSubmit={sendDeviceInfo}/>
 
-      <Form src="https://xojgwtxalpylmpd.form.io/customer" onCustomEvent={() => alert.show('Oh look, an alert!')} />
-      {/* <FormBuilder form={{ display: 'form' }} onChange={(schema) => console.log(schema) (submit)="onSubmit($event.data)}
-       />
-     */}
 
-      <button
-        onClick={() => {
-        //  Alert.alert("Are you sure?", "You can't be serious.")
+      {/* <h2>Fill your personal informations</h2>
 
-        }}
-      >
-        Show Alert
-  </button>
+      <Form src="https://xojgwtxalpylmpd.form.io/registeration" onSubmit={(event) => handleChange(event)} onClick={click} /> */}
+
     </div>
     {/* <Footer /> */}
   </div>
 )
-const callAPI = () => {
-  console.log("fhrtyrtyrty")
 
-  // fetch('https://dev10api.ayainnovation.com/api/user/login', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Accept': 'application/json',
-  //     'Content-Type': 'application/json',
-  //     'Authorization': ' hQCOKs75uoYxakySqIA7qrjzdj2Z9PYn'
-  //   },
-  //   body: JSON.stringify({
-  //     deviceId: '5b4588ba164e12f6345',
-  //     deviceName: 'Samsung Galaxy J7',
-  //     os: 'Android',
-  //     osVersion: '10.14.6',
-  //     appVersion: '1.0.0',
-  //     firebaseToken: 'ej59N6AC8xA:APA91bGuLKLQg-9q8jsBOUra0MIkmz4agl2IgtRfnyAARtv0Ws0oAVI7YMtYBJkQKqoIxTrTyHRdLXpmyf1YidAPzkZxuAbcHls9pVnbTQHIl_znXnCL1Cfw2-2PN1sI1hPFBCLncr0b', // required: true
-  //     lat: '20.560796',
-  //     long: '106.076115'
-  //   })
-  // });
+const sendDeviceInfo = () => {
+  // Simple POST request with a JSON body using fetch
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic hQCOKs75uoYxakySqIA7qrjzdj2Z9PYn'
+    },
+    body: JSON.stringify({
+      deviceId: '5b4588ba164e12f6345',
+      deviceName: 'Samsung Galaxy J7',
+      os: 'Android',
+      osVersion: '10.14.6',
+      appVersion: '1.0.0',
+      firebaseToken: 'ej59N6AC8xA:APA91bGuLKLQg-9q8jsBOUra0MIkmz4agl2IgtRfnyAARtv0Ws0oAVI7YMtYBJkQKqoIxTrTyHRdLXpmyf1YidAPzkZxuAbcHls9pVnbTQHIl_znXnCL1Cfw2-2PN1sI1hPFBCLncr0b', // required: true
+      lat: '20.560796',
+      long: '106.076115'
+    })
+  };
+  fetch('https://dev10api.ayainnovation.com/api/user/sendDeviceInfo', requestOptions)
+    .then(response => response.json())
+    .then(data => console.log("Response", data));
 }
+
+const login = () => {
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Basic hQCOKs75uoYxakySqIA7qrjzdj2Z9PYn'
+    },
+    body: JSON.stringify({
+      phone: '09789789789',
+      password: '123457',
+      deviceId: '5b4588ba164e12f6345',
+      firebaseToken: 'ej59N6AC8xA:APA91bGuLKLQg-9q8jsBOUra0MIkmz4agl2IgtRfnyAARtv0Ws0oAVI7YMtYBJkQKqoIxTrTyHRdLXpmyf1YidAPzkZxuAbcHls9pVnbTQHIl_znXnCL1Cfw2-2PN1sI1hPFBCLncr0b'
+    })
+  };
+  fetch('https://dev10api.ayainnovation.com/api/user/login', requestOptions)
+    .then(response => response.json())
+    .then(data => console.log("Response", data));
+
+}
+
+const handleChange = (event) => {
+  console.log("Change", event)
+}
+
+function WarningBanner(props) {
+  return (
+    <div className={props.warn ? "warning": "hide"}>
+      <h1> Warning! </h1>
+    </div>
+  );
+}
+
+
 
 
 export default App
